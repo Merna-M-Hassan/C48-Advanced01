921ff81f12532d08575e32d23d97633e766ac3b9
Merna-M-Hassan <merna.abdelfatah.hassan@gmail.com>
2026-08-28 19:06:08 +0300

    Question 2)

--- Program.cs
+++ Program.cs
@@ -17,6 +17,21 @@
              * - Enforce compiler type safety (check for values are compatible with the defined data type)
              */
             #endregion
+
+            #region Q2)
+            // Write a generic class Container<T> with Add and Get methods.
+
+            //Container<string> names = new Container<string>(5);
+
+            //names.Add("Ali");
+            //names.Add("Mariam");
+            //names.Add("Faya");
+            //names.Add("Lara");
+
+            //Console.WriteLine(names.Get(1));        
+            #endregion
+
+
         }
     }
 }
