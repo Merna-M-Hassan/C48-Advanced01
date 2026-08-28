d4b00b6077d89316953a8fc65bb7ff16dd6ffbc5
Merna-M-Hassan <merna.abdelfatah.hassan@gmail.com>
2026-08-28 16:09:37 +0300

    Question 1)

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1)
+            //What is a generic class? Why use generics?
+
+            /*
+             * Is a class without a defined data type (string/double/int/..etc) for its field, until using it. Instead, it has a placeholder for
+             * the data type is defined when using the class. Once an object/instance is created from the generic class, the data type must be defined.
+             * 
+             * Use generics because we need to apply the following: 
+             * - To make code reusability:  reuse the class in different data types.
+             * - Need all fields/properties to have the same defined data type.
+             * - Enforce compiler type safety (check for values are compatible with the defined data type)
+             */
+            #endregion
         }
     }
 }
